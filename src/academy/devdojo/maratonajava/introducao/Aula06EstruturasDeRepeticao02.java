ee367bdb08274b4613de4c2099712a977f2a15c8
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-03-18 12:25:41 -0300

    EstruturasDeRepetição1,2 :grin:

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 1000000
+        int n = 0;
+        for(n = 0; n <= 1000000; n++){
+            if(n%2==0){
+            System.out.println(n);
+            }
+        }
+    }
+}
